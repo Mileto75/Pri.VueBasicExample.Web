9cd90684ea9f0803188cfa861864f1a0325894b6
Miledi75 <mileto.di.marco@howest.be>
2023-05-08 09:26:56 +0200

    Add project files.

---
+++ Pri.VueBasic.Web/wwwroot/js/coindesk/coindesk.js
@@ -0,0 +1,20 @@
+﻿let app = new Vue({
+    el: "#coindesk",
+    data: {
+        pageTitle: "Coindesk App",
+        data: null,
+        error: null,
+        loaded: false,
+        hasError: false,
+        url: "https://api.coindesk.com/v1/bpi/currentprice.json",
+    },
+    created: async function () {
+        //await fetch(this.url)
+        //    .then(response => response.json())
+        //    .then(data => {this.data = data.bpi });
+        await axios.get(this.url)
+            .then(response => this.data = response.data.bpi)
+            .catch(error => { this.error = error; this.hasError = true })
+            .finally(() => { this.loaded=true});
+    }
+});
---
+++ Pri.VueBasic.Web/wwwroot/js/intro/intro.js
@@ -0,0 +1,33 @@
+﻿//init vue engine
+let app = new Vue({
+    el: "#app",
+    data: {
+        pageTitle: "Our first cool Vue app!",
+        rawHtml: "<b>this is raw html</b>",
+        url: "https://www.gazzetta.it",
+        hoverText: "Football news",
+        formVisible: false,
+        person: {
+            name: null,
+            city: null,
+        },
+        visible: true,
+        people: [
+            {name:"Karel", city:"Gent"},
+            {name:"Jan", city:"Blankenberge"},
+            {name:"Cindy", city:"Brugge"},
+        ],
+        inputValue: "Change me",
+    },
+    methods: {
+        toggleVisible: function () {
+            this.visible = !this.visible;     
+        },
+        showForm: function (){
+            this.formVisible = !this.formVisible;
+        },
+        addPerson: function () {
+            this.people.push(this.person);
+        },
+    },
+});
